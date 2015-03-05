2aa793bb1f10515054f8d04ac476c8f162a37e6d
Matt Sell <matt.sell@improvingenterprises.com>
2015-03-04 22:01:55 -0600

    got one automated test and snapshot

--- demo/demo/describe_registration.cs
+++ demo/demo/describe_registration.cs
@@ -1,24 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Machine.Specifications;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 
 namespace demo
 {
     public class describe_user_registration
     {
+        Establish context = () =>
+        {
+            _driver = new FirefoxDriver();
+        };
+
+        Cleanup teardown = () => _driver.Quit();
+        
         public class when_user_enters_invalid_email
         {
-            It should_display_email_error;                        
+            Establish context = () =>
+            {
+                _driver.Navigate().GoToUrl(url);
+                _driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(2));
+            };
+
+            Because of = () =>
+            {
+                var query = _driver.FindElement(By.Name("email"));
+                query.SendKeys("cookieMonster");
+                _error = _driver.FindElementByXPath("//p").Text;                                               
+            };
+
+            It should_display_email_error = () =>
+            {
+                _error.Should().Contain("Email is invalid");
+                _driver.GetScreenshot().SaveAsFile("invalidEmail.png", ImageFormat.Png);
+            };
+            static IWebElement _emailError;
+            static string _error;
         }
 
         public class when_user_enters_valid_email
         {
             It should_not_display_email_error;
         }
-        
-        static string url = "http://quickstart-frontend.herokuapp.com/";
+
+        public class when_user_has_password_missmatch
+        {
+            It should_display_missmatch_error;
+        }
+
+        static readonly string url = "http://quickstart-frontend.herokuapp.com/#/register";
+        static FirefoxDriver _driver;
     }
 }
